ce8765c15a16217a746fa2f0ffef20516d3bed9b
ivanLotoff <adsege2017@gmail.com>
2022-10-02 16:58:09 +0300

    убрал базу

--- Dockerfile
+++ Dockerfile
@@ -3,30 +3,6 @@ COPY src /home/app/src
 COPY pom.xml /home/app
 WORKDIR /home/app
 
-ARG DB_PORT
-ENV DB_PORT ${DB_PORT}
-
-ARG DB_SCHEMA
-ENV DB_SCHEMA ${DB_SCHEMA}
-
-ARG DB_NAME
-ENV DB_NAME ${DB_NAME}
-
-ARG DB_USER
-ENV DB_USER ${DB_USER}
-
-ARG DB_PASSWORD
-ENV DB_PASSWORD ${DB_PASSWORD}
-
-ARG DB_CONTAINER_NAME
-ENV DB_CONTAINER_NAME ${DB_CONTAINER_NAME}
-
 RUN mvn clean install
 
 CMD mvn spring-boot:run
-#RUN mvn -f /home/app/pom.xml clean package
-#
-#FROM openjdk:11
-#COPY --from=build /home/app/target/docker-template-0.0.1-SNAPSHOT.jar /usr/local/lib/demo.jar
-#EXPOSE 8080
-#ENTRYPOINT ["java","-jar","/usr/local/lib/demo.jar"]
--- src/main/kotlin/ru/ivan/dockertemplate/DockerTemplateApplication.kt
+++ src/main/kotlin/ru/ivan/dockertemplate/DockerTemplateApplication.kt
@@ -1,13 +1,10 @@
 package ru.ivan.dockertemplate
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-import org.springframework.web.bind.annotation.*
-import javax.persistence.Entity
-import javax.persistence.Id
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DockerTemplateApplication
@@ -19,24 +16,7 @@ fun main(args: Array<String>) {
 @RestController
 @RequestMapping("/api")
 class CityController {
-	@Autowired
-	private lateinit var cityRepository: CityRepository
 
-	@GetMapping("/all")
-	fun findAll(): List<City> = cityRepository.findAll()
-
-	@GetMapping("/{id}")
-	fun findById(@PathVariable("id") id: Int) = cityRepository.findById(id)
-
-	@PostMapping("/save")
-	fun save(@RequestBody city: City) = cityRepository.save(city)
-
-	@DeleteMapping("/{id}")
-	fun deleteById(@PathVariable("id") id: Int) = cityRepository.deleteById(id)
+	@GetMapping("/hello")
+	fun greet() = "hello!"
 }
-
-@Repository
-interface CityRepository: JpaRepository<City, Int>
-
-@Entity
-data class City(@Id val id: Int, val name: String)
